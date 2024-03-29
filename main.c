5dc2f5d03ed62f6a0b4cb9b3e83badcc497b10ca
Sadek-21 <mohamed20000327@gmail.com>
2024-03-28 23:25:09 -0700

    main

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "monty.h"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    stack_t *stack = NULL;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, file)) != -1)
+    {
+        printf("Read line: %s", line); 
+    }
+
+    free(line);
+    fclose(file);
+    return EXIT_SUCCESS;
+}
+i
